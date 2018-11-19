533b5b9c656bded71f9022c27afbc1d8d8bc81ab
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-11-19 14:34:50 +0530

    Add chatkit to server

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Chatkit = require('pusher-chatkit-server')
+const keys = require('./keys')
 
 const app = express()
+
+const chatkit = new Chatkit.default({
+  instanceLocator: keys.chatkitInstance,
+  key: keys.chatkitSecret
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+
+app.post('/users', (req, res) => {
+  const {username} = req.body
+
+  chatkit.createUser({
+    name: username,
+    id: username
+  })
+  .then(() => res.sendStatus(201))
+  .catch(err => {
+    if (err.error_type === 'services/chatkit/user/user_already_exists') {
+      res.sendStatus(200)
+    } else {
+      res.sendStatus(err.statusCode).json(err)
+    }
+  })
+})
 
 const PORT = 3001
 app.listen(PORT, err => {
