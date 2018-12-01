e4e46227a00b950d40a441de285d22f96adc0404
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-12-01 23:58:34 +0530

    Add Message component

---
+++ src/components/MessageList/Message.jsx
@@ -0,0 +1,19 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+export default class Message extends PureComponent {
+  static propTypes = {
+    messageSender: PropTypes.string,
+    messageText: PropTypes.string
+  }
+
+  render() {
+    const { messageSender, messageText } = this.props
+    return (
+      <li className='message'>
+        <span className='message-sender'>{messageSender}</span>
+        <p className='message-text'>{messageText}</p>
+      </li>
+    )
+  }
+}
