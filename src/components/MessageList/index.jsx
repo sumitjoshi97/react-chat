3ec17c2963407a28233e9cbae1b5e9b47d783cce
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-12-01 23:58:20 +0530

    Reafactor : converted to functional component

--- src/components/MessageList/index.jsx
+++ src/components/MessageList/index.jsx
@@ -1,18 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
+import Message from './Message'
 import './styles.css'
 
-export default class MessageList extends Component {
-  render() {
-    return (
-      <ul className='message-list'>
-        {this.props.messages.map((message, index) => (
-          <li key={index} className='message'>
-            <span className='message-sender'>{message.senderId}</span>
-            <p className='message-text'>{message.text}</p>
-          </li>
-        ))}
-      </ul>
-    )
-  }
+const MessageList = ({ messages }) => {
+  return (
+    <ul className='message-list'>
+      {messages.map(message => (
+        <Message
+          key={message.id}
+          messageSender={message.senderId}
+          messageText={message.text}
+        />
+      ))}
+    </ul>
+  )
 }
+
+MessageList.propTypes = {
+  messages: PropTypes.array
+}
+
+export default MessageList
