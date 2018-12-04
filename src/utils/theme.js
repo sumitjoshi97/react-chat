fad38739b5efefa8bfc34a28edf258391b0ce6af
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-12-04 12:29:36 +0530

    Add themes

---
+++ src/utils/theme.js
@@ -0,0 +1,31 @@
+const DARK_THEME = {
+  primaryBackground: '#181b1f',
+  secondaryBackground: '#2c2f33',
+  tertiaryBackground: '#21252b',
+  fontPrimary: '#fff',
+  fontSecondary: '#adadad',
+  memberOnline: '#2e2',
+  memberOffline: '#2e2e2e'
+}
+
+const LIGHT_BLUE_THEME = {
+  primaryBackground: '#80b7f5',
+  secondaryBackground: '#e6e6e6',
+  tertiaryBackground: '#dbdbdb',
+  fontPrimary: '#000',
+  fontSecondary: '#30373f',
+  memberOnline: '#227093',
+  memberOffline: '#d2dae2'
+}
+
+const LIGHT_RED_THEME = {
+  primaryBackground: '#ff7979',
+  secondaryBackground: '#e6e6e6',
+  tertiaryBackground: '#dbdbdb',
+  fontPrimary: '#000',
+  fontSecondary: '#30373f',
+  memberOnline: '#b33939',
+  memberOffline: '#d2dae2'
+}
+
+export { DARK_THEME, LIGHT_BLUE_THEME, LIGHT_RED_THEME }
