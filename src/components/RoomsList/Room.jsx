333705d533fedc76a9abbeb4c21ce7197b0ebbe7
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-12-04 12:30:32 +0530

    Add styles to component

--- src/components/RoomsList/Room.jsx
+++ src/components/RoomsList/Room.jsx
@@ -1,16 +1,42 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import Radium from 'radium'
 
-export default class Room extends Component {
+class Room extends Component {
   static propTypes = {
-    roomName: PropTypes.string
+    roomName: PropTypes.string,
+    selectionColor: PropTypes.string.isRequired,
+    hoverColor: PropTypes.string.isRequired
   }
 
   render() {
+    const { isCurrentRoom, selectionColor, hoverColor } = this.props
+
+    const styles = {
+      roomStyles: {
+        ':hover': {
+          background: hoverColor
+        }
+      },
+      currentRoomStyles: {
+        background: selectionColor
+      }
+    }
+
+    const roomStyle = isCurrentRoom
+      ? { ...styles.roomStyles, ...styles.currentRoomStyles }
+      : styles.roomStyles
+
     return (
-      <li className={this.props.classes} onClick={this.props.setCurrentRoom}>
+      <li
+        className='list-item'
+        style={roomStyle}
+        onClick={this.props.setCurrentRoom}
+      >
         {this.props.roomName}
       </li>
     )
   }
 }
+
+export default Radium(Room)
