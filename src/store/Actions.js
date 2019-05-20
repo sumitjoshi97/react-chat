ec845d451dc9caaee4bb0936f360ed172c083a07
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-05-20 19:22:37 +0530

    chore: add chatkit actions

--- src/store/Actions.js
+++ src/store/Actions.js
@@ -11,7 +11,6 @@ const loginFailure = () => ({
 })
 
 export const onUsernameSubmit = (username, dispatch) => {
-  console.log('action', username)
   const URL = `${process.env.REACT_APP_SERVER_URI}/users`
   fetch(URL, {
     method: 'POST',
@@ -21,8 +20,7 @@ export const onUsernameSubmit = (username, dispatch) => {
     body: JSON.stringify({ username }),
   })
     .then(res => {
-      console.log('success')
-      return dispatch(loginSuccess(username))
+      dispatch(loginSuccess(username))
     })
     .catch(err => dispatch(loginFailure))
 }
@@ -35,8 +33,76 @@ const changeTheme = theme => ({
   theme,
 })
 
-export const onChangeTheme = (theme, currentTheme) => dispatch => {
+export const onChangeTheme = (theme, currentTheme, dispatch) => {
   if (theme !== currentTheme) {
-    dispatch({ changeTheme })
+    dispatch(changeTheme(theme))
   }
+}
+
+/////////////////////////////////////////////////////////////////
+//chatkit actions
+
+const setCurrentRoom = room => ({
+  type: 'SET_CURRENT_ROOM',
+  currentRoom: room,
+})
+
+const setCurrentUser = user => ({
+  type: 'SET_CURRENT_USER',
+  currentUser: user,
+})
+
+const addMessage = message => ({
+  type: 'ADD_MESSAGE',
+  message,
+})
+
+export const connectToChatkit = (chatManager, dispatch) => {
+  let currentUser
+  let currentRoom
+
+  chatManager
+    .connect({
+      onAddedToRoom: () => this.forceUpdate(),
+      onUserJoinedRoom: () => this.forceUpdate(),
+    })
+    .then(user => {
+      currentUser = user
+      console.log(user)
+      dispatch(setCurrentUser(currentUser))
+    })
+    .then(() => {
+      if (currentUser.rooms.length > 0) {
+        currentUser.rooms.map(room =>
+          subscribeUserToRoom(currentUser, room.id, dispatch)
+        )
+      } else {
+        //subscribe to general room for first time user
+        subscribeUserToRoom(
+          currentUser,
+          process.env.REACT_APP_GENERAL_ROOM_ID,
+          dispatch
+        )
+      }
+    })
+    .then(() => {
+      currentRoom = currentUser.rooms[0]
+      dispatch(setCurrentRoom(currentRoom))
+    })
+    .then(() => {
+      //fetchMessagesForCurrentRoom(currentRoom)
+    })
+    .catch(err => console.error(err))
+}
+
+export const subscribeUserToRoom = (user, roomId, dispatch) => {
+  return user.subscribeToRoomMultipart({
+    roomId,
+    messageLimit: 30,
+    hooks: {
+      onMessage: message => dispatch(addMessage(message)),
+      onPresenceChange: () => this.forceUpdate(),
+      onUserJoined: () => this.forceUpdate(),
+    },
+  })
 }
