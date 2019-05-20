a92de9fd7e4db7535ee7b2d577f8fcf96461e14f
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-05-20 19:18:05 +0530

    refactor: remove commented code

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -6,34 +6,10 @@ import ChatScreen from '../views/ChatScreen'
 import { Store } from '../store/Store'
 
 const App = () => {
-  // state = {
-  //   currentUsername: '',
-  //   currentScreen: 'LoginScreen',
-  // }
-
-  // onUsernameSubmit = username => {
-  //   fetch(`${process.env.REACT_APP_SERVER_URI}/users`, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({ username }),
-  //   })
-  //     .then(response =>
-  //       this.setState({
-  //         currentUsername: username,
-  //         currentScreen: 'ChatScreen',
-  //       })
-  //     )
-  //     .catch(err => console.error(err))
-  // }
-
   const { state } = useContext(Store)
 
   return (
-    <div className="app">
-      {state.isLogin === true ? <ChatScreen /> : <UserLogin />}
-    </div>
+    <div className="app">{state.isAuth ? <ChatScreen /> : <UserLogin />}</div>
   )
 }
 
