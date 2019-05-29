834ad221823bc93a74bf2ad49f5c441fc2810c19
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-05-29 15:11:17 +0530

    refactor: convert class component to functional

--- src/components/MembersList/index.jsx
+++ src/components/MembersList/index.jsx
@@ -1,60 +1,60 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import Member from './Member'
 import Scrollbar from '../Scrollbar'
 
-export default class MembersList extends Component {
-  static propTypes = {
-    members: PropTypes.array,
-    toggleDialog: PropTypes.func.isRequired,
-    theme: PropTypes.object.isRequired
+const MembersList = ({ members, theme, ...props }) => {
+  const styles = {
+    listStyles: {
+      background: theme.primaryBackground,
+      color: theme.fontPrimary,
+    },
+    buttonStyles: {
+      background: theme.secondaryBackground,
+      color: theme.fontPrimary,
+    },
   }
 
-  renderMembers = () => {
-    const { members, theme } = this.props
+  const renderMembers = () => {
     if (members) {
-      return this.props.members.map(member => (
-        <Member
-          key={member.id}
-          memberName={member.name}
-          memberPresence={member.presence}
-          memberOfflineColor={theme.memberOffline}
-          memberOnlineColor={theme.memberOnline}
-        />
-      ))
+      return members.map(member => {
+        return (
+          <Member
+            key={member.id}
+            memberName={member.name}
+            memberPresence={member.presence}
+            memberOfflineColor={theme.memberOffline}
+            memberOnlineColor={theme.memberOnline}
+          />
+        )
+      })
     }
   }
 
-  render() {
-    const { theme } = this.props
-
-    const styles = {
-      listStyles: {
-        background: theme.primaryBackground,
-        color: theme.fontPrimary
-      },
-      buttonStyles: {
-        background: theme.secondaryBackground,
-        color: theme.fontPrimary
-      }
-    }
-    return (
-      <div className='column' style={styles.listStyles}>
-        <div className='list-header'>Members list</div>
-        <ul className='list'>
-          <Scrollbar thumbColor={theme.secondaryBackground} autoHide>
-            {this.renderMembers()}
-          </Scrollbar>
-        </ul>
-        <button
-          className='add-btn'
-          style={styles.buttonStyles}
-          onClick={() => this.props.toggleDialog('member to room')}
-        >
-          Add
-        </button>
-      </div>
-    )
-  }
+  return (
+    <div className="column" style={styles.listStyles}>
+      <div className="list-header">Members list</div>
+      <ul className="list">
+        <Scrollbar thumbColor={theme.secondaryBackground} autoHide>
+          {renderMembers()}
+        </Scrollbar>
+      </ul>
+      <button
+        className="add-btn"
+        style={styles.buttonStyles}
+        onClick={() => props.handleDialog('member to room')}
+      >
+        Add
+      </button>
+    </div>
+  )
 }
+
+MembersList.propTypes = {
+  // members: PropTypes.array,
+  // toggleDialog: PropTypes.func.isRequired,
+  // theme: PropTypes.object.isRequired,
+}
+
+export default MembersList
