f52fc06721845b08c6b1b0ff464b99ebc99cff23
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-05-29 15:14:52 +0530

    refactor: convert class component to functional

--- src/components/MessageList/index.jsx
+++ src/components/MessageList/index.jsx
@@ -1,48 +1,81 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import React, { useEffect, useRef } from 'react'
 
 import Message from './Message'
 import Scrollbar from '../Scrollbar'
 import './styles.css'
 
-export default class MessageList extends Component {
-  static propTypes = {
-    messages: PropTypes.array,
-    theme: PropTypes.object.isRequired
+// export default class MessageList extends Component {
+//   static propTypes = {
+//     messages: PropTypes.array,
+//     theme: PropTypes.object.isRequired,
+//   }
+
+//   UNSAFE_componentWillUpdate = () => {
+//     const node = ReactDOM.findDOMNode(this)
+//     this.shouldScrollToBottom =
+//       node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
+//   }
+
+//   componentDidUpdate = () => {
+//     const node = ReactDOM.findDOMNode(this)
+//     node.scrollTop = node.scrollHeight
+//   }
+
+// renderMessages = () => {
+//   const { messages, theme } = this.props
+
+//   return messages.map(message => (
+//     <Message
+//       key={message.id}
+//       messageSender={message.senderId}
+//       messageText={message.text}
+//       senderStyle={theme.fontSecondary}
+//       messageStyle={theme.fontPrimary}
+//     />
+//   ))
+// }
+
+// render() {
+//   return (
+//     <ul className="message-list">
+//       <Scrollbar thumbColor={this.props.theme.primaryBackground}>
+//         {this.renderMessages()}
+//         <div ref={messageBottom} />
+//       </Scrollbar>
+//     </ul>
+//   )
+// }
+// }
+
+const MessageList = ({ messages, theme }) => {
+  let messageEndRef = useRef(null)
+
+  const scrollToBottom = () => {
+    messageEndRef.current.scrollIntoView({ behavior: 'smooth' })
   }
 
-  UNSAFE_componentWillUpdate = () => {
-    const node = ReactDOM.findDOMNode(this)
-    this.shouldScrollToBottom =
-      node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
-  }
+  useEffect(scrollToBottom, [messages])
 
-  componentDidUpdate = () => {
-    const node = ReactDOM.findDOMNode(this)
-    node.scrollTop = node.scrollHeight
-  }
-
-  renderMessages = () => {
-    const { messages, theme } = this.props
-
+  const renderMessages = () => {
     return messages.map(message => (
       <Message
         key={message.id}
         messageSender={message.senderId}
-        messageText={message.text}
+        messageText={message.parts[0].payload.content}
         senderStyle={theme.fontSecondary}
         messageStyle={theme.fontPrimary}
       />
     ))
   }
-  render() {
-    return (
-      <ul className='message-list'>
-        <Scrollbar thumbColor={this.props.theme.primaryBackground}>
-          {this.renderMessages()}
-        </Scrollbar>
-      </ul>
-    )
-  }
+
+  return (
+    <ul className="message-list">
+      <Scrollbar thumbColor={theme.primaryBackground}>
+        {renderMessages()}
+        <div ref={messageEndRef} />
+      </Scrollbar>
+    </ul>
+  )
 }
+
+export default MessageList
