f415377a06ae891b979f768844bb9f69bb5cc90d
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-05-29 15:12:47 +0530

    chore: update store and reducer

--- src/store/Store.jsx
+++ src/store/Store.jsx
@@ -1,8 +1,15 @@
 import React, { createContext, useReducer } from 'react'
+import * as themes from '../utils/theme'
 
 const initialState = {
-  isLogin: false,
-  username: '',
+  isAuth: false,
+  userId: '',
+  currentUser: {},
+  currentRoom: {},
+  messages: [],
+  currentUsersTyping: [],
+  forceUpdateCount: 0,
+  theme: themes.BLUE_THEME,
 }
 
 export const Store = createContext(initialState)
@@ -10,7 +17,37 @@ export const Store = createContext(initialState)
 const reducer = (state, action) => {
   switch (action.type) {
     case 'LOGIN_SUCCESS':
-      return { ...state, isLogin: true, username: action.username }
+      return { ...state, isAuth: true, userId: action.username }
+    case 'SET_CURRENT_USER':
+      return { ...state, currentUser: action.currentUser }
+    case 'LOGOUT_SUCCESS':
+      return initialState
+    case 'SET_CURRENT_ROOM':
+      return { ...state, currentRoom: action.currentRoom }
+    case 'SET_MESSAGES':
+      return { ...state, messages: action.messages }
+    case 'SET_CLEAR_MESSAGES':
+      return { ...state, messages: [] }
+    case 'ADD_MESSAGE':
+      return { ...state, messages: [...state.messages, action.message] }
+    case 'SET_CLEAR_CURRENT_USERS_TYPING':
+      return { ...state, currentUsersTyping: [] }
+    case 'ADD_TO_CURRENT_USERS_TYPING':
+      return {
+        ...state,
+        currentUsersTyping: [...state.currentUsersTyping, action.user],
+      }
+    case 'REMOVE_FROM_CURRENT_USERS_TYPING':
+      return {
+        ...state,
+        currentUsersTyping: state.currentUsersTyping.filter(
+          user => user.name !== action.user.name
+        ),
+      }
+    case 'CHANGE_THEME':
+      return { ...state, theme: action.theme }
+    case 'FORCE_UPDATE':
+      return { ...state, forceUpdateCount: state.forceUpdateCount + 1 }
     default:
       return state
   }
