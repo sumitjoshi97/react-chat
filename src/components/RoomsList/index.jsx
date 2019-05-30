d95bdb9b94ad211571d321dd388915aa43d97564
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-05-30 15:37:23 +0530

    refactor: convert class component to functional

--- src/components/RoomsList/index.jsx
+++ src/components/RoomsList/index.jsx
@@ -1,23 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-
+import { handleCurrentRoom, addRoom } from '../../store/Actions'
 import Room from './Room'
 import Scrollbar from '../Scrollbar'
 
-export default class RoomsList extends Component {
-  static propTypes = {
-    theme: PropTypes.object.isRequired
+const RoomsList = ({ currentUser, currentRoom, theme, ...props }) => {
+  const styles = {
+    listStyles: {
+      background: theme.primaryBackground,
+      color: theme.fontPrimary,
+    },
+    buttonStyles: {
+      background: theme.secondaryBackground,
+      color: theme.fontPrimary,
+    },
   }
 
-  renderRooms = () => {
-    const { rooms, theme } = this.props
-    if (rooms) {
-      return this.props.rooms.map(room => (
+  const renderRooms = () => {
+    if (currentUser.hasOwnProperty('roomStore')) {
+      return Object.values(currentUser.roomStore.rooms).map(room => (
         <Room
           key={room.id}
           roomName={room.name}
-          isCurrentRoom={this.props.currentRoom.id === room.id}
-          setCurrentRoom={() => this.props.setCurrentRoom(room.id)}
+          isCurrentRoom={currentRoom.id === room.id}
+          setCurrentRoom={() => {
+            currentRoom.id !== room.id &&
+              handleCurrentRoom(currentUser, room.id, props.dispatch)
+          }}
           selectionColor={theme.secondaryBackground}
           hoverColor={theme.tertiaryBackground}
         />
@@ -25,35 +34,23 @@ export default class RoomsList extends Component {
     }
   }
 
-  render() {
-    const { theme } = this.props
-
-    const styles = {
-      listStyles: {
-        background: theme.primaryBackground,
-        color: theme.fontPrimary
-      },
-      buttonStyles: {
-        background: theme.secondaryBackground,
-        color: theme.fontPrimary
-      }
-    }
-    return (
-      <div className='column' style={styles.listStyles}>
-        <div className='list-header'>Your Rooms</div>
-        <ul className='list'>
-          <Scrollbar thumbColor={theme.secondaryBackground} autoHide>
-            {this.renderRooms()}
-          </Scrollbar>
-        </ul>
-        <button
-          className='add-btn'
-          style={styles.buttonStyles}
-          onClick={() => this.props.toggleDialog('room')}
-        >
-          create Room
-        </button>
-      </div>
-    )
-  }
+  return (
+    <div className="column" style={styles.listStyles}>
+      <div className="list-header">Your Rooms</div>
+      <ul className="list">
+        <Scrollbar thumbColor={theme.secondaryBackground} autoHide>
+          {renderRooms()}
+        </Scrollbar>
+      </ul>
+      <button
+        className="add-btn"
+        style={styles.buttonStyles}
+        onClick={() => props.handleDialog('room')}
+      >
+        create Room
+      </button>
+    </div>
+  )
 }
+
+export default RoomsList
