d5697f3d94d14d91da70cce60f7b1605703e9a6c
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-05-30 15:36:59 +0530

    chore: remove commented code

--- src/components/MessageList/index.jsx
+++ src/components/MessageList/index.jsx
@@ -4,49 +4,6 @@ import Message from './Message'
 import Scrollbar from '../Scrollbar'
 import './styles.css'
 
-// export default class MessageList extends Component {
-//   static propTypes = {
-//     messages: PropTypes.array,
-//     theme: PropTypes.object.isRequired,
-//   }
-
-//   UNSAFE_componentWillUpdate = () => {
-//     const node = ReactDOM.findDOMNode(this)
-//     this.shouldScrollToBottom =
-//       node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
-//   }
-
-//   componentDidUpdate = () => {
-//     const node = ReactDOM.findDOMNode(this)
-//     node.scrollTop = node.scrollHeight
-//   }
-
-// renderMessages = () => {
-//   const { messages, theme } = this.props
-
-//   return messages.map(message => (
-//     <Message
-//       key={message.id}
-//       messageSender={message.senderId}
-//       messageText={message.text}
-//       senderStyle={theme.fontSecondary}
-//       messageStyle={theme.fontPrimary}
-//     />
-//   ))
-// }
-
-// render() {
-//   return (
-//     <ul className="message-list">
-//       <Scrollbar thumbColor={this.props.theme.primaryBackground}>
-//         {this.renderMessages()}
-//         <div ref={messageBottom} />
-//       </Scrollbar>
-//     </ul>
-//   )
-// }
-// }
-
 const MessageList = ({ messages, theme }) => {
   let messageEndRef = useRef(null)
 
